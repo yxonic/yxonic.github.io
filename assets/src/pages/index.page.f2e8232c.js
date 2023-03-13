cb421f9e015cc7598ee63b84d22bae653c856439
yxonic <yxonic@users.noreply.github.com>
2023-03-13 15:53:30 +0000

    deploy: 65a538808524b42f991031725d46c7255485eacf

---
+++ assets/src/pages/index.page.f2e8232c.js
@@ -0,0 +1,50 @@
+import{h as e}from"../../chunks/6432436b.js";const a=e("div",{},[e("h1",{},["Yu Yin",e("span",{class:"text-2xl"},["（阴钰）"])]),`
+`,e("img",{src:"/avatar.jpg",alt:"avatar",class:"h-20 w-20 rounded-full shadow-inner float-right -mt-16"},[void 0]),`
+`,e("h2",{},["Introduction"]),`
+`,e("ul",{},[`
+`,e("li",{},[`I am an AI researcher that is good at:
+`,e("ul",{},[`
+`,e("li",{},[e("strong",{},["Learning, thinking and sharing"]),". Driven by curiosity. Care about innovative and impactful ideas. Love to share what I learn and think."]),`
+`,e("li",{},[e("strong",{},["Put thoughts into reality"]),". Solid systems and engineering background. Open-source practitioner."]),`
+`]),`
+`]),`
+`,e("li",{},["My research interests include: natural language processing, user modeling and personalization, code intelligence, and educational applications."]),`
+`]),`
+`,e("h2",{},["Education"]),`
+`,e("ul",{},[`
+`,e("li",{},[`
+`,e("p",{},[e("em",{},["Sept 2017 – July 2023 (estimated)"])]),`
+`,e("p",{},["School of Computer Science, USTC"]),`
+`]),`
+`,e("li",{},[`
+`,e("p",{},[e("em",{},["Sept 2013 – July 2017"])]),`
+`,e("p",{},["School of Computer Science, USTC"]),`
+`]),`
+`]),`
+`,e("h2",{},["Experiences"]),`
+`,e("h2",{},["Publications"]),`
+`,e("p",{},["See ",e("a",{href:"/publications"},["full list"]),"."]),`
+`,e("h2",{},["Projects"]),`
+`,e("p",{},["See ",e("a",{href:"/projects"},["full list"]),"."]),`
+`,e("h2",{},["Services & Awards"]),`
+`,e("ul",{},[`
+`,e("li",{},["Program Committee of SIGIR '23, ACL '23."]),`
+`,e("li",{},["National Scholarship for Postgraduates, ",e("em",{},["Sept 2018"]),"."]),`
+`,e("li",{},["Student Travel Award for KDD'18, ",e("em",{},["Aug 2018"]),"."]),`
+`,e("li",{},["Excellent Bachelor Dissertation Award for USTC, ",e("em",{},["Jun 2017"]),"."]),`
+`,e("li",{},["First Prize, Parallel Application Challenge (PAC2015), ",e("em",{},["Nov 2015"]),"."]),`
+`,e("li",{},["First Prize, The Second Student RDMA Programming Contest, ",e("em",{},["Oct 2014"]),"."]),`
+`,e("li",{},["Bronze Prize, Chinese National Olympiad in Informatics (NOI), ",e("em",{},["Aug 2012"]),"."]),`
+`]),`
+`,e("h2",{},["Talks"]),`
+`,e("ul",{},[`
+`,e("li",{},[e("a",{href:"http://202.38.75.5/shares/%E5%A4%A7%E5%88%86%E4%BA%AB/2022-12-02-%E9%98%B4%E9%92%B0-JAX.pdf"},["JAX: Compile the Future of Deep Learning to Present"]),", ",e("em",{},["Dec 2022"]),"."]),`
+`,e("li",{},[e("a",{href:"http://202.38.75.5/shares/%E5%B0%8F%E5%88%86%E4%BA%AB/2020-09-17-%E9%98%B4%E9%92%B0-%E5%A6%82%E4%BD%95%E6%9C%89%E9%80%BB%E8%BE%91.pdf"},["How to be Logical"]),", ",e("em",{},["Sept 2020"]),"."]),`
+`,e("li",{},[e("a",{href:""},["Computers: Past, Present and Future"]),", ",e("em",{},["Nov 2019"]),"."]),`
+`,e("li",{},[e("a",{href:"http://202.38.75.5/shares/%E5%A4%A7%E5%88%86%E4%BA%AB/2019-11-12-%E9%98%B4%E9%92%B0-Continuous%20Control%20Basics%20and%20Beyond.pptx"},["Continuous Control: Basics and Beyond"]),", ",e("em",{},["Nov 2019"]),"."]),`
+`,e("li",{},[e("a",{href:"http://202.38.75.5/shares/%E5%B0%8F%E5%88%86%E4%BA%AB/2019-04-02-%E9%98%B4%E9%92%B0-fret_slides.pdf"},["fret: Framework for Reproducible ExperimenTs"]),", ",e("em",{},["Apr 2019"]),"."]),`
+`,e("li",{},[e("a",{href:"http://202.38.75.5/shares/%E5%B0%8F%E5%88%86%E4%BA%AB/2017-10-31-rl-%E9%98%B4%E9%92%B0.pdf"},["Reinforcement Learning: Basics"]),", ",e("em",{},["Oct 2017"]),"."]),`
+`,e("li",{},["C++11"]),`
+`,e("li",{},[e("a",{href:"http://ustc-lambda.github.io/files/scala-slides-0416.pdf"},["SCAlable LAnguage Scala"]),", ",e("em",{},["Apr 2016"]),"."]),`
+`,e("li",{},[e("a",{href:"https://ftp.lug.ustc.edu.cn/%E6%B4%BB%E5%8A%A8/2014.09.20_SFD/%E5%B9%BB%E7%81%AF%E7%89%87/sage.pdf"},["What is Sage"]),", Software Freedom Day, ",e("em",{},["Sept 2014"]),"."]),`
+`])]),i={},n={render(){return a}};n.__hmrId="ad8e9c64";n.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/pages/index.page.md";typeof __VUE_HMR_RUNTIME__<"u"&&(__VUE_HMR_RUNTIME__.createRecord(n.__hmrId,n),(void 0).accept(({default:t})=>{__VUE_HMR_RUNTIME__.rerender(t.__hmrId,t.render)}));export{n as default,i as frontmatter,a as nodes};
---
+++ assets/src/pages/projects.page.7b40ee9d.js
@@ -0,0 +1,35 @@
+import{h as n}from"../../chunks/6432436b.js";const i=n("div",{},[n("h2",{},["Projects"]),`
+`,n("h3",{},["Machine Learning"]),`
+`,n("ul",{},[`
+`,n("li",{},["fret"]),`
+`,n("li",{},["labeling"]),`
+`,n("li",{},["EduNLP"]),`
+`,n("li",{},["EduCDM"]),`
+`,n("li",{},["EduKTM"]),`
+`]),`
+`,n("h3",{},["Content Sharing"]),`
+`,n("ul",{},[`
+`,n("li",{},["vmark"]),`
+`,n("li",{},["vpages"]),`
+`,n("li",{},["vslides (WIP)"]),`
+`]),`
+`,n("h3",{},["Infrastructure"]),`
+`,n("ul",{},[`
+`,n("li",{},["srun"]),`
+`,n("li",{},["rcommunity (WIP)"]),`
+`]),`
+`,n("h3",{},["Product"]),`
+`,n("ul",{},[`
+`,n("li",{},["CODIA: team leader, architect"]),`
+`,n("li",{},["Luna: tech consultant"]),`
+`]),`
+`,n("h3",{},["Games"]),`
+`,n("ul",{},[`
+`,n("li",{},["PK2048"]),`
+`,n("li",{},["Pecker (WIP)"]),`
+`]),`
+`,n("h3",{},["Misc"]),`
+`,n("ul",{},[`
+`,n("li",{},["ycc1"]),`
+`,n("li",{},["ychat"]),`
+`])]),l={},e={render(){return i}};e.__hmrId="66b17dce";e.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/pages/projects.page.md";typeof __VUE_HMR_RUNTIME__<"u"&&(__VUE_HMR_RUNTIME__.createRecord(e.__hmrId,e),(void 0).accept(({default:r})=>{__VUE_HMR_RUNTIME__.rerender(r.__hmrId,r.render)}));export{e as default,l as frontmatter,i as nodes};
