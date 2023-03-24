0c78aea3cecd54af9ea592a843ad6d5afc9ee3d7
yxonic <yxonic@users.noreply.github.com>
2023-03-24 14:55:57 +0000

    deploy: 1f00d6745955f17d5982de7cca45e1e4b026655a

---
+++ assets/src/slides/open-source-llms.page.91a2ddc1.js
@@ -0,0 +1,61 @@
+import{t as i,_ as a}from"../../chunks/afcc8c88.js";import{h as n}from"../../chunks/ded0b38f.js";const c={title:"Open-Source PLMs",theme:"./theme/basic",layout:"cover"},e=[];e.push({component:()=>n("div",{},[n("h1",{},["Open-Source PLMs"]),`
+`,n("p",{},["@yxonic & @lem0nle"])]),config:{title:"Open-Source PLMs",theme:"./theme/basic",layout:"cover"}});e.push({component:()=>n("div",{},[n("h1",{},["Contents"]),`
+`,n("ul",{},[`
+`,n("li",{},["What is PLM?"]),`
+`,n("li",{},["Open-source PLMs"]),`
+`,n("li",{},[`Learn from code
+`,n("ul",{},[`
+`,n("li",{},["model architecture"]),`
+`,n("li",{},["training"]),`
+`,n("li",{},[`running
+`,n("ul",{},[`
+`,n("li",{},["run LLaMA locally with ",n("code",{},["llama.cpp"])]),`
+`,n("li",{},["projects based on OpenAI API"]),`
+`]),`
+`]),`
+`]),`
+`]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["What is PLM?"]),`
+`,n("ul",{},[`
+`,n("li",{},["recent AI advances: ",n("a",{href:"http://dustintran.com/blog/ai-advances"},["http://dustintran.com/blog/ai-advances"])]),`
+`,n("li",{},["PLMs pre GPT-3: ",n("a",{href:"https://towardsdatascience.com/pre-trained-language-models-simplified-b8ec80c62217"},["blog"])]),`
+`,n("li",{},[n("a",{href:"https://www.topbots.com/leading-nlp-language-models-2020/"},["10 Leading Language Models For NLP In 2022"])]),`
+`,n("li",{},[n("a",{href:"https://jingfengyang.github.io/gpt"},["Why did all of the public reproduction of GPT-3 fail?"])]),`
+`,n("li",{},["using PLMs: ",n("a",{href:"https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/"},["Prompt Engineering"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["ChatGPT"]),`
+`,n("ul",{},[`
+`,n("li",{},["ChatGPT history review: ",n("a",{href:"https://baijiahao.baidu.com/s?id=1758688291629341140"},["ChatGPT 的前世今生"])]),`
+`,n("li",{},[n("a",{href:"https://yaofu.notion.site/GPT-3-5-360081d91ec245f29029d37b54573756"},["拆解追溯 GPT-3.5 各项能力的起源"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Open-Source LLMs"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://github.com/google-research/t5x"},["T5X"]),", ",n("a",{href:"https://huggingface.co/google/flan-t5-base"},["Flan-T5"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/facebookresearch/llama"},["LLaMA"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/tatsu-lab/stanford_alpaca"},["alpaca"]),", ",n("a",{href:"https://crfm.stanford.edu/2023/03/13/alpaca.html"},["website"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/THUDM/GLM"},["GLM"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/THUDM/ChatGLM-6B"},["ChatGLM"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Model Architecture"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://github.com/google-research/t5x"},["T5"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/facebookresearch/llama"},["LLaMA"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/THUDM/GLM"},["GLM"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Training"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://github.com/google-research/t5x"},["T5 training"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/philschmid/deep-learning-pytorch-huggingface"},["fine-tuning flan-t5"])]),`
+`,n("li",{},["fine-tuning LLaMA: ",n("a",{href:"https://github.com/tatsu-lab/stanford_alpaca"},["alpaca"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Fine-tuning efficiency"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://www.philschmid.de/fine-tune-flan-t5-peft"},["https://www.philschmid.de/fine-tune-flan-t5-peft"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/thunlp/OpenDelta"},["OpenDelta"]),", ",n("a",{href:"https://mp.weixin.qq.com/s/R4pJORJ4H2DbWcHHt40YOA"},["news"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Multi-modal?"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://shikun.io/projects/prismer"},["https://shikun.io/projects/prismer"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Efficient running"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://github.com/ggerganov/llama.cpp"},["llama.cpp"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["Projects based on API"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://github.com/sigoden/aichat"},["https://github.com/sigoden/aichat"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/zurawiki/gptcommit"},["https://github.com/zurawiki/gptcommit"])]),`
+`,n("li",{},[n("a",{href:"https://github.com/keijiro/AICommand"},["https://github.com/keijiro/AICommand"])]),`
+`])]),config:void 0});const t={setup(){return()=>n(a,{pages:e,theme:i})}};t.__hmrId="2f1e2fef";t.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/slides/open-source-llms.page.mdx";export{t as default,c as frontmatter,e as pages};
