19c7c00e0ac8daa039e1b11fcac79e85c3c40efe
yxonic <yxonic@users.noreply.github.com>
2023-03-28 13:28:17 +0000

    deploy: 3aa6b30e5b89359dc7923d28b8561a4ffed73302

--- assets/src/slides/career.page.b63579f0.js
+++ assets/src/slides/career.page.b63579f0.js
@@ -1,11 +1,25 @@
-import{t as o,_ as t}from"../../chunks/afcc8c88.js";import{h as n}from"../../chunks/ded0b38f.js";const c={title:"Career",theme:"./theme/basic",layout:"cover"},e=[];e.push({component:()=>n("div",{},[n("h1",{},["Career"]),`
-`,n("p",{},["@yxonic"])]),config:{title:"Career",theme:"./theme/basic",layout:"cover"}});e.push({component:()=>n("div",{},[n("h1",{},["个人陈述"]),`
+import{t,_ as o}from"../../chunks/afcc8c88.js";import{h as n}from"../../chunks/ded0b38f.js";const r={title:"Career",theme:"./theme/basic",layout:"cover"},e=[];e.push({component:()=>n("div",{},[n("h1",{},["Career"]),`
+`,n("p",{},["@yxonic"])]),config:{title:"Career",theme:"./theme/basic",layout:"cover"}});e.push({component:()=>n("div",{},[n("h1",{},["准备工作"]),`
+`,n("ul",{},[`
+`,n("li",{},[n("a",{href:"https://sites.google.com/view/datascience-cheat-sheets"},["Preparation cheet sheet"])]),`
+`,n("li",{},[n("a",{href:"https://towardsdatascience.com/types-of-machine-learning-interviews-and-how-to-ace-them-51587a95f847"},["ML interview"])]),`
+`,n("li",{},[n("a",{href:"https://towardsdatascience.com/how-to-prepare-for-a-behavioral-soft-skills-interview-cheat-sheet-9347aaeaef82"},["Soft skill interview"])]),`
+`,n("li",{},[n("a",{href:"https://towardsdatascience.com/what-is-machine-learning-system-design-interview-and-how-to-prepare-for-it-537d1271d754"},["ML system interview"])]),`
+`,n("li",{},[n("a",{href:"https://aqeel-anwar.medium.com/machine-learning-resume-that-got-me-shortlisted-for-meta-microsoft-nvidia-uber-samsung-intel-220761c1a850"},["Resume"])]),`
+`,n("li",{},[n("a",{href:"https://aqeel-anwar.medium.com/the-dos-and-donts-of-a-software-engineer-resume-to-get-you-shortlisted-for-interviews-more-7d6a926156c0"},["Resume dos and don'ts"])]),`
+`,n("li",{},[n("a",{href:"https://arxiv.org/abs/2201.00650"},["deep learning interviews"])]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["个人陈述"]),`
 `,n("ul",{},[`
 `,n("li",{},["中科大陈恩红老师博士生"]),`
 `,n("li",{},["教育组组长"]),`
 `,n("li",{},["研究兴趣和方向"]),`
 `,n("li",{},["系统"]),`
-`])]),config:void 0});e.push({component:()=>n("h1",{},["职业需求和倾向"]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["腾讯 AILab 绝悟团队"]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["TODO"]),`
+`,n("ul",{},[`
+`,n("li",{},["腾讯换成机器学习？换成实习生？"]),`
+`,n("li",{},["问童世炜 0-1 的工作是什么，IEG 有没有机会。"]),`
+`,n("li",{},["字节找人问问情况"]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["腾讯 AILab 绝悟团队"]),`
 `,n("p",{},[`【团队介绍】
 腾讯绝悟 AI 团队在游戏 AI 领域具有雄厚的科研和工程实力，该团队研发的游戏 AI 在以王者荣耀为代表的多款游戏中达到了极高的竞技水平，击败了人类职业玩家，并具有成熟的商业化链条。近年来，团队在 NeurIPS, ICML, ICLR, AAAI, IJCAI, TNNLS 等人工智能会议/期刊上发表了多篇文章，在学术界及工业界均产生了较大的影响力。`]),`
 `,n("p",{},[`【岗位职责】
@@ -21,7 +35,13 @@ import{t as o,_ as t}from"../../chunks/afcc8c88.js";import{h as n}from"../../chu
 `,n("li",{},["了解绝悟团队"]),`
 `,n("li",{},["熟悉经历中的强化学习部分"]),`
 `,n("li",{},["multi-agent RL"]),`
-`])]),config:void 0});e.push({component:()=>n("h1",{},["微信搜一搜"]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["准备工作"]),`
+`])]),config:void 0});e.push({component:()=>n("h1",{},["微信搜一搜"]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["微信搜一搜：问题"]),`
+`,n("ul",{},[`
+`,n("li",{},["focus"]),`
+`,n("li",{},["大模型冲击？"]),`
+`,n("li",{},["留用考核，每年什么时候"]),`
+`,n("li",{},["地理位置：广州和深圳，业务对比，生活对比"]),`
+`])]),config:void 0});e.push({component:()=>n("div",{},[n("h1",{},["准备工作"]),`
 `,n("ul",{},[`
 `,n("li",{},["熟悉讯飞经历"]),`
 `,n("li",{},["搜索引擎"]),`
@@ -40,4 +60,4 @@ import{t as o,_ as t}from"../../chunks/afcc8c88.js";import{h as n}from"../../chu
 `,n("li",{},["具有良好的问题分析和问题解决能力，对相关技术领域具有敏锐的洞察力和较强的学习能力，能积极应对有挑战性的工作；"]),`
 `,n("li",{},["乐观积极，具有良好的团队合作精神和较强的沟通意识和能力；"]),`
 `,n("li",{},["有丰富的搜索引擎、推荐系统或广告系统相关工作经验最佳； 有较强工程解决能力最佳。"]),`
-`])]),config:void 0});const i={setup(){return()=>n(t,{pages:e,theme:o})}};i.__hmrId="71f983b4";i.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/slides/career.page.mdx";export{i as default,c as frontmatter,e as pages};
+`])]),config:void 0});const i={setup(){return()=>n(o,{pages:e,theme:t})}};i.__hmrId="71f983b4";i.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/slides/career.page.mdx";export{i as default,r as frontmatter,e as pages};
