13cfaad8f6a1dfa86f73f551a400b8147498ec66
yxonic <yxonic@users.noreply.github.com>
2023-07-25 03:27:55 +0000

    deploy: ada04a9e4943fe56f413ac07a99c4b83320c40fa

---
+++ assets/entries/src_slides_rust-for-work.page.1914b05a.js
@@ -0,0 +1,18 @@
+import{t as r,_ as e}from"../chunks/chunk-aec62340.js";import{h as o}from"../chunks/chunk-5cf4a0e1.js";const u={title:"Rust for Work",theme:"./theme/basic",layout:"cover"},t=[];t.push({component:()=>o("div",{},[o("h1",{},["Rust for Work"]),`
+`,o("p",{},["@yxonic"])]),config:{title:"Rust for Work",theme:"./theme/basic",layout:"cover"}});t.push({component:()=>o("div",{},[o("h1",{},["参考"]),`
+`,o("p",{},["Rust language"]),`
+`,o("ul",{},[`
+`,o("li",{},[o("a",{href:"https://rust-lang-nursery.github.io/rust-cookbook/intro.html"},["https://rust-lang-nursery.github.io/rust-cookbook/intro.html"])]),`
+`]),`
+`,o("p",{},["Data processing"]),`
+`,o("ul",{},[`
+`,o("li",{},[o("a",{href:"https://jorgecarleitao.github.io/arrow2/main/docs/arrow2/"},["https://jorgecarleitao.github.io/arrow2/main/docs/arrow2/"])]),`
+`]),`
+`,o("p",{},["Engineering"]),`
+`,o("ul",{},[`
+`,o("li",{},["bevy"]),`
+`])]),config:void 0});t.push({component:()=>o("div",{},[o("h1",{},["类比"]),`
+`,o("ul",{},[`
+`,o("li",{},["C++, Java, Python"]),`
+`,o("li",{},["结构化数据库->非结构化数据库->结构化数据库加入非结构化特性"]),`
+`])]),config:void 0});const n={setup(){return()=>o(e,{pages:t,theme:r})}};n.__hmrId="0b9bc61e";n.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/slides/rust-for-work.page.mdx";export{n as default,u as frontmatter,t as pages};
