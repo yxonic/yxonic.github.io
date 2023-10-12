ecaca1cc4f6f48ce7607a94b6bae728f03b90cb9
Yin Yu <yinyu3@xiaohongshu.com>
2023-10-12 17:38:18 +0800

    init vslides live

---
+++ src/vslides-live/index.page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fixed top-0 left-0 right-0 bottom-0 grid grid-cols-2">
+    <page-container class="bg-black">
+      <end-page />
+    </page-container>
+    <div>test</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PageContainer from "@vslides/core/components/page/PageContainer.vue";
+import EndPage from "@vslides/core/components/page/EndPage.vue";
+import "@vslides/core/style/base.css";
+</script>
