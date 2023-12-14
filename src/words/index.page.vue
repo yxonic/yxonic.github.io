1567ca6062df6f177038a140f2f4a925ed3388f9
Yin Yu <yinyu3@xiaohongshu.com>
2023-12-14 15:43:23 +0800

    add primevue

---
+++ src/words/index.page.vue
@@ -0,0 +1,24 @@
+<template>
+  <Card>
+    <template #title>猫</template>
+    <template #content>
+      <Accordion>
+        <AccordionTab v-for="tab in tabs" :key="tab.word" :header="tab.word">
+          <p class="m-0">{{ tab.definition }}</p>
+        </AccordionTab>
+      </Accordion>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import Card from "primevue/card";
+import Accordion from "primevue/accordion";
+import AccordionTab from 'primevue/accordiontab';
+
+const tabs = [
+  {word: 'cat', definition: '猫'},
+  {word: 'shark', definition: '鲨鱼'},
+  {word: 'elephant', definition: '大象'},
+]
+</script>
--- vpage/renderer/_default.page.client.ts
+++ vpage/renderer/_default.page.client.ts
@@ -1,12 +1,16 @@
+import PrimeVue from "primevue/config";
 import { createApp } from "../lib/app";
 import type { PageContext } from "../lib/context";
 
 import "uno.css";
 import "katex/dist/katex.css";
+import "primevue/resources/themes/viva-light/theme.css";
+
 import "./global.css";
 
 function render(pageContext: PageContext) {
   const app = createApp(pageContext);
+  app.use(PrimeVue, { ripple: true });
   app.mount("#app");
 }
 
