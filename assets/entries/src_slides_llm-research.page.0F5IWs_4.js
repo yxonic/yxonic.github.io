e3307eb9a85cacc522d1a9e332aada9bf458049d
yxonic <yxonic@users.noreply.github.com>
2023-12-27 09:01:33 +0000

    deploy: 4c14700b53816ba63d0dddfa1bc9deeee88b24e9

---
+++ assets/entries/src_slides_llm-research.page.0F5IWs_4.js
@@ -0,0 +1,55 @@
+import{t as i,_ as o}from"../chunks/chunk-4VtuNNwy.js";import"../chunks/chunk-OO0dnDxZ.js";import{h as e}from"../chunks/chunk-zQavgIRI.js";import"../chunks/chunk-9rAokE7f.js";import"../chunks/chunk-x3n3nnut.js";const m={title:"LLM Research Ideas",theme:"./theme/basic",layout:"cover"},n=[];n.push({component:()=>e("div",{},[e("h1",{},["LLM Research Ideas"]),`
+`,e("p",{},["@yxonic"])]),config:{title:"LLM Research Ideas",theme:"./theme/basic",layout:"cover"}});n.push({component:()=>e("div",{},[e("h1",{},["Retrieval Augmented Generation (RAG)"]),`
+`,e("p",{},["Challenges:"]),`
+`,e("ul",{},[`
+`,e("li",{},[`retrieve more relevant and informative documents
+`,e("ul",{},[`
+`,e("li",{},["search engine"]),`
+`,e("li",{},["knowledge base"]),`
+`,e("li",{},["embedding"]),`
+`]),`
+`]),`
+`,e("li",{},[`make better use of context
+`,e("ul",{},[`
+`,e("li",{},["prompt"]),`
+`,e("li",{},["memory"]),`
+`,e("li",{},["reflection"]),`
+`]),`
+`]),`
+`,e("li",{},[`apply to a wider range of tasks
+`,e("ul",{},[`
+`,e("li",{},["QA"]),`
+`,e("li",{},["assistant"]),`
+`,e("li",{},["recommendation"]),`
+`,e("li",{},["content creation"]),`
+`]),`
+`]),`
+`])]),config:void 0});n.push({component:()=>e("div",{},[e("h1",{},["Interactive Content Creation with TreeWriter"]),`
+`,e("ul",{},[`
+`,e("li",{},[`interactive content creation
+`,e("ul",{},[`
+`,e("li",{},["interactive content: users ",e("strong",{},["read"])," (consume content) and ",e("strong",{},["choose"])," (interact with content)"]),`
+`,e("li",{},["challenge: it have been expensive for content creators to produce multi-branch, more complex content"]),`
+`,e("li",{},["solution: utilize LLM to assist/automate the creation process"]),`
+`]),`
+`]),`
+`,e("li",{},[`compare: traditional content
+`,e("ul",{},[`
+`,e("li",{},["users will have a non-linear, interactive experience that may increase their engagement and attachment"]),`
+`,e("li",{},["creation of this kind of content has much more challenges than traditional content creation"]),`
+`]),`
+`]),`
+`,e("li",{},[`compare: dialog-based interactive systems
+`,e("ul",{},[`
+`,e("li",{},["cost less to interact with the system: users don't ",e("em",{},["write"])]),`
+`,e("li",{},["have more control over the content"]),`
+`]),`
+`]),`
+`,e("li",{},[`examples:
+`,e("ul",{},[`
+`,e("li",{},["interactive fiction"]),`
+`,e("li",{},["NPC conversation in games"]),`
+`,e("li",{},["easy assistant (we can see them as interactive user manuals)"]),`
+`]),`
+`]),`
+`])]),config:void 0});const t={setup(){return()=>e(o,{pages:n,theme:i})}};t.__hmrId="3821c6c9";t.__file="/home/runner/work/yxonic.github.io/yxonic.github.io/src/slides/llm-research.page.mdx";export{t as default,m as frontmatter,n as pages};
